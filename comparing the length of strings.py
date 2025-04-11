ce46d6f83eb1ecb6f7ba5ad4dc362f1868762274
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-04-11 22:22:30 +0530

    comparing the length of strings.py

--- comparing the length of strings.py
+++ comparing the length of strings.py
@@ -1,4 +1,4 @@
-tring1 = input("Enter first string: ")
+string1 = input("Enter first string: ")
 string2 = input("Enter second string: ")
 if len(str1) > len(str2):
     print(f'"{str1}" is longer than "{str2}".')
